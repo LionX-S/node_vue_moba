18ce1dbb05636400a7252430680aca472e544dd0
LionX-S <13141096848@163.com>
2023-03-03 10:17:10 +0800

    feat:添加英雄表单校验规则

--- admin/src/components/Hero/HeroCreate.vue
+++ admin/src/components/Hero/HeroCreate.vue
@@ -4,25 +4,35 @@
 		<el-form
 			label-width="120px"
 			label-position="left"
-			@submit.native.prevent="save">
+			:model="heroes"
+			ref="hero"
+			:rules="rules">
 			<el-tabs type="border-card">
 				<el-tab-pane label="基础信息">
-					<el-form-item label="英雄名称">
+					<el-form-item
+						label="英雄名称"
+						prop="name">
 						<el-input
 							v-model="heroes.name"
 							placeholder="请输入英雄名称"></el-input>
 					</el-form-item>
-					<el-form-item label="英雄称号">
+					<el-form-item
+						label="英雄称号"
+						prop="nickName">
 						<el-input
 							v-model="heroes.nickName"
 							placeholder="请输入英雄称号"></el-input>
 					</el-form-item>
-					<el-form-item label="英雄故事">
+					<el-form-item
+						label="英雄故事"
+						prop="describes">
 						<el-input
 							v-model="heroes.describes"
 							placeholder="请输入英雄故事"></el-input>
 					</el-form-item>
-					<el-form-item label="英雄分类">
+					<el-form-item
+						label="英雄分类"
+						prop="category">
 						<el-select
 							v-model="heroes.category"
 							placeholder="请选择">
@@ -37,7 +47,9 @@
 					<el-form-item label="难度等级">
 						<el-rate v-model="heroes.score"></el-rate>
 					</el-form-item>
-					<el-form-item label="英雄头像">
+					<el-form-item
+						label="英雄头像"
+						prop="avatar">
 						<el-upload
 							class="avatar-uploader"
 							:action="$http.defaults.baseURL + '/upload'"
@@ -103,8 +115,14 @@
 			<div class="buttonRow">
 				<el-button
 					type="primary"
-					native-type="submit"
+					@click="submitForm('hero', save)"
 					>保存</el-button
+				>
+				<el-button
+					v-if="!id"
+					@click="resetVal"
+					type="warning"
+					>重置</el-button
 				>
 				<el-button
 					type="danger"
@@ -131,10 +149,39 @@
 					score: 0,
 					skills: []
 				},
-				heroCategoryOpt: []
+				heroCategoryOpt: [],
+				rules: {
+					name: [
+						{ required: true, message: "请输入英雄名称", trigger: "blur" }
+					],
+					nickName: [
+						{ required: true, message: "请输入英雄称号", trigger: "blur" }
+					],
+					avatar: [
+						{ required: true, message: "请上传图片", trigger: "change" }
+					],
+					describes: [
+						{ required: true, message: "请输入英雄故事", trigger: "blur" }
+					],
+					category: [
+						{ required: true, message: "请选择英雄分类", trigger: "change" }
+					]
+				}
 			};
 		},
 		methods: {
+			async resetVal() {
+				if (!this.id) {
+					this.heroes.avatar !== "" && (await deleteImage(this.heroes.avatar));
+					this.heroes.skills.length &&
+						this.heroes.skills.map((item) => {
+							if (item.skillImage !== "") {
+								deleteImage(skillImage);
+							}
+						});
+				}
+				this.resetForm("hero");
+			},
 			addSkills() {
 				this.heroes.skills.push({
 					skillName: "",
@@ -147,14 +194,6 @@
 				this.heroes.skills.splice(index, 1);
 			},
 			async save() {
-				// 填写数据校验
-				if (!this.heroes.name.trim()) {
-					this.$message({
-						type: "warning",
-						message: "请填写正确数据！"
-					});
-					return;
-				}
 				// 如果是新建则走新建api，如果是编辑走编辑api
 				let res;
 				if (this.id) {
@@ -190,7 +229,7 @@
 			// 编辑分类功能
 			async getHeroesById(id) {
 				const res = await this.$http.get(`rest/heroes/${id}`);
-				const { body, message, code } = res.data;
+				const { body } = res.data;
 				this.heroes = {
 					...this.heroes,
 					...body[0],
@@ -200,7 +239,7 @@
 			// 查询英雄大分类下所有分类
 			async getCategoryByHero(categoryName) {
 				const res = await this.$http.get(`getChildCategory/${categoryName}`);
-				const { body, message, code } = res.data;
+				const { body } = res.data;
 				this.heroCategoryOpt = body;
 			},
 
