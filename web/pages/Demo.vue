ba3b9617c6fdd51b7ec53d65924c40b8bb4e4400
LionX-S <13141096848@163.com>
2023-03-08 09:51:38 +0800

    feat:初始化web 采用nuxt3

---
+++ web/pages/Demo.vue
@@ -0,0 +1,12 @@
+<template lang="">
+  <div>
+    <h1>demo</h1>
+    <nuxt-link to="/index">Index page</nuxt-link>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+  
+</style>
---
+++ web/pages/Index.vue
@@ -0,0 +1,12 @@
+<template lang="">
+  <div>
+    <h1>index page</h1>
+    <nuxt-link to="/demo">demo page</nuxt-link>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+  
+</style>
